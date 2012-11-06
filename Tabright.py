1c7a4c063cefeb2e547266c6682e744da3a546e5
Martijn Gussekloo <gus@stylishmedia.com>
2012-11-06 19:18:29 +0100

    Better implementation using view settings

--- Tabright.py
+++ Tabright.py
@@ -3,10 +3,12 @@ import sublime, sublime_plugin
 class TabrightEvent(sublime_plugin.EventListener):
 
 	def move_tab_right(self,view):
-		window = sublime.active_window()
-		rightIndex = len(window.views()) - 1
-		window.set_view_index(view, window.active_group(), rightIndex)
-		window.run_command("select_by_index", {"index": rightIndex})
+		if not view.settings().get("Tabright_processed"):
+  			view.settings().set("Tabright_processed", True)
+			window = sublime.active_window()
+			rightIndex = len(window.views()) - 1
+			window.set_view_index(view, window.active_group(), rightIndex)
+			window.run_command("select_by_index", {"index": rightIndex})
 
 	def on_new(self,view):
 		self.move_tab_right(view)
@@ -15,10 +17,9 @@ class TabrightEvent(sublime_plugin.EventListener):
 		window = sublime.active_window()
 		is_preview = window and view.file_name() not in [file.file_name() for file in window.views()]
 		if not is_preview:
-			if view.id()==window.active_view().id():
-				self.move_tab_right(view)
+			self.move_tab_right(view)
 
 	def on_close(self,view):
 		window = sublime.active_window()
 		group,index = window.get_view_index(view)
-		window.run_command("select_by_index", {"index": index+1})
+		window.run_command("select_by_index", {"index": index-1})
