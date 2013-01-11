c66ddb65fdbb0bb4c8914d46aa612e01ef9773bc
Martijn Gussekloo <gus@stylishmedia.com>
2013-01-11 18:13:24 +0100

    Added option for far_left

--- Tabright.py
+++ Tabright.py
@@ -2,36 +2,55 @@ import sublime, sublime_plugin
 
 class TabrightEvent(sublime_plugin.EventListener):
 
-	def move_tab_right(self,view):
+	def move_tab(self,view):
 		if not view.settings().get("Tabright_processed"):
+			view.settings().set("Tabright_processed", True)
+
+			settings = sublime.load_settings("Tabright.sublime-settings")
+			open_new_tabs_at = settings.get("open_new_tabs_at")
+
 			window = sublime.active_window()
-  			view.settings().set("Tabright_processed", True)
 			group,index = window.get_view_index(view)
-			rightIndex = len(window.views_in_group(group)) - 1
-			window.set_view_index(view, group, rightIndex)
+
+			if (open_new_tabs_at == "far_right"):
+				newIndex = len(window.views_in_group(group)) - 1
+			elif (open_new_tabs_at == "far_left"):
+				newIndex = 0;
+
+			window.set_view_index(view, group, newIndex)
 			window.focus_group(group)
 			window.focus_view(view)
 
 	def on_new(self,view):
 	    def callback(view=view):
-			return self.move_tab_right(view)
-	    sublime.set_timeout(callback, 10)
+			return self.move_tab(view)
+	    sublime.set_timeout(callback, 1)
 
 	def on_load(self,view):
 		window = sublime.active_window()
 		is_preview = window and view.file_name() not in [file.file_name() for file in window.views()]
 		if not is_preview:
-			self.move_tab_right(view)
+			def callback(view=view):
+				return self.move_tab(view)
+			sublime.set_timeout(callback, 1)
 
 	def on_close(self,view):
 		window = sublime.active_window()
 		group,index = window.get_view_index(view)
+
+		def callback(index=index):
+			return self.focus_tab(index)
+		sublime.set_timeout(callback, 1)
+
+	def focus_tab(self,index):
+		window = sublime.active_window()
+		group = window.active_group()
 		views = window.views_in_group(group)
-		lenViews = len(views)-1 #we subtract one because we are closing a view
-		focusIndex = index + 1 #to the right
+		lenViews = len(views)
 
-		if focusIndex > lenViews-1:
-			focusIndex = lenViews-1
+		if (lenViews>0):
+			if index > lenViews-1:
+				index = lenViews-1
 
-		window.focus_group(group)
-		window.focus_view(views[focusIndex])
+			window.focus_group(group)
+			window.focus_view(views[index])
