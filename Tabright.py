81fa87953a5000da4af5e3313baa7da44d163a5b
Martijn Gussekloo <gus@stylishmedia.com>
2013-01-14 13:34:51 +0100

    Hacked around various timing issues with the ST2 events

--- Tabright.py
+++ Tabright.py
@@ -4,17 +4,18 @@ class TabrightEvent(sublime_plugin.EventListener):
 
 	def move_tab(self,view):
 		if not view.settings().get("Tabright_processed"):
-			view.settings().set("Tabright_processed", True)
+			if not view.settings().get("Tabright_processed"):
+				view.settings().set("Tabright_processed", True)
 
-			settings = sublime.load_settings("Tabright.sublime-settings")
+				settings = sublime.load_settings("Tabright.sublime-settings")
 			open_new_tabs_at = settings.get("open_new_tabs_at")
 
 			window = sublime.active_window()
 			group,index = window.get_view_index(view)
 
-			if (open_new_tabs_at == "far_right"):
-				newIndex = len(window.views_in_group(group)) - 1
-			elif (open_new_tabs_at == "far_left"):
+			newIndex = len(window.views_in_group(group)) - 1
+
+			if (open_new_tabs_at == "far_left"):
 				newIndex = 0;
 
 			window.set_view_index(view, group, newIndex)
@@ -24,23 +25,30 @@ class TabrightEvent(sublime_plugin.EventListener):
 	def on_new(self,view):
 	    def callback(view=view):
 			return self.move_tab(view)
-	    sublime.set_timeout(callback, 1)
+	    sublime.set_timeout(callback,1)
 
-	def on_load(self,view):
+	def on_activated(self,view):
+		if not view.settings().get("Tabright_processed"):
+			def callback(view=view):
+				return self.view_presented(view)
+			sublime.set_timeout(callback,100)
+
+	def view_presented(self,view):
 		window = sublime.active_window()
-		is_preview = window and view.file_name() not in [file.file_name() for file in window.views()]
-		if not is_preview:
+		is_preview = view.file_name() is not "None" and window and view.file_name() not in [file.file_name() for file in window.views()]
+		if is_preview:
+			return
+		else:
 			def callback(view=view):
 				return self.move_tab(view)
-			sublime.set_timeout(callback, 1)
+			sublime.set_timeout(callback,1)
 
 	def on_close(self,view):
 		window = sublime.active_window()
 		group,index = window.get_view_index(view)
-
 		def callback(index=index):
 			return self.focus_tab(index)
-		sublime.set_timeout(callback, 1)
+		sublime.set_timeout(callback,1)
 
 	def focus_tab(self,index):
 		window = sublime.active_window()
